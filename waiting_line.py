b9078738b82d4103725377bb4d9ab0d39c24f951
Vítor Sales <vitorsaless10@gmail.com>
2026-02-21 15:01:29 -0300

    killed class because it makes no sense on this of code

--- waiting_line.py
+++ waiting_line.py
@@ -1,24 +1,27 @@
 #here's to generate me a number that always going up
-class number_generator():
-    def generate_number(self):
+'''class number_generator():
+    def generate_number():
         self.count = 0
-        self.caller = str
-
-    def user_input():
+        self.caller = str'''
+#killed this class because it makes no sense on this code
+#but i am storing here just in case
+def user_input():
         reset_point = 101
-        count = 0
-        user = str(input("Tirar senha Y Cancelar N: ")).lower().strip()
+        count = 0 
         while True:
             try:
+                user = str(input("Tirar senha Y Cancelar N: ")).lower().strip()
                 if user == "Y":
-                        print(f"Your current number {count}")
-                        count =+ 1
+                    count in range(0,100)
+                    count =+1
+                    print(f"Your current number {count}")
                 elif count >= reset_point:
                     print("Reseting counting")
                 elif user == "N": 
                     break
             except ValueError:
                  print("Para imprimir uma senha pressione Y se nao deseja pegar uma, pressione N")
+            return user 
 
-    user_input()        
-number_generator()
+user_input()        
+#number_generator()
