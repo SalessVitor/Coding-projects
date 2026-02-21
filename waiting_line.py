145134396d367d5ddd32354d6a8c2a2d2010319f
Vitor Sales <vitorsaless10@gmail.com>
2026-02-21 20:03:39 -0300

    undestanded a crucial conception to make code run happy to know that

--- waiting_line.py
+++ waiting_line.py
@@ -1,31 +1,18 @@
 #here's to generate me a number that always going up
-'''class number_generator():
-    def generate_number():
-        self.count = 0
-        self.caller = str'''
-#killed this class because it makes no sense on this code
-#but i am storing here just in case
 def user_input():
         reset_point = 3
+        reset_value = 0
         count = 0 
         while True:
-            #try:
                 user = str(input("Tirar senha Y Cancelar N: ")).lower().strip()
                 if user == "y":
-                    #count in range(0,100)y  
                     user = print(f"Your current number {count}")
-                    count +=1                
-                if count >= reset_point:
-                    print(f"Reseting counting {count}")
-                    #count = 0
-                    count +=1
-                if user == "n": 
-                    break
+                    count +=1 
                 else:
                      print("Digite Y ou N")
-        '''except ValueError:
-                 print("Para imprimir uma senha pressione Y se nao deseja pegar uma, pressione N")
-            return user''' 
-
+                if count >= reset_point:
+                    reset_value 
+                    print(f"Reseting counting {reset_value}")
+                if user == "n": 
+                    break
 user_input()        
-#number_generator()
