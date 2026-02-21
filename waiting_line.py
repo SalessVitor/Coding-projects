6a6647499a971a9449a935e3c131819bc3397098
Vítor Sales <vitorsaless10@gmail.com>
2026-02-21 15:47:00 -0300

    input with count worked begginig to work on the reset/break part

--- waiting_line.py
+++ waiting_line.py
@@ -6,22 +6,25 @@
 #killed this class because it makes no sense on this code
 #but i am storing here just in case
 def user_input():
-        reset_point = 101
+        
         count = 0 
         while True:
-            try:
+            #try:
                 user = str(input("Tirar senha Y Cancelar N: ")).lower().strip()
-                if user == "Y":
-                    count in range(0,100)
-                    count =+1
-                    print(f"Your current number {count}")
+                if user == "y":
+                    #count in range(0,100)y  
+                    user = print(f"Your current number {count}")
+                    count +=1
+                    reset_point = 101
                 elif count >= reset_point:
                     print("Reseting counting")
-                elif user == "N": 
+                elif user == "n": 
                     break
-            except ValueError:
+                else:
+                     print("Digite Y ou N")
+        '''except ValueError:
                  print("Para imprimir uma senha pressione Y se nao deseja pegar uma, pressione N")
-            return user 
+            return user''' 
 
 user_input()        
 #number_generator()
