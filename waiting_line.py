bc5d2b63dd7d1255e8db31e8b89882ceda8be18e
Vítor Sales <vitorsaless10@gmail.com>
2026-02-22 18:41:22 -0300

    changing generate number loop logic from 'if' to match case

--- waiting_line.py
+++ waiting_line.py
@@ -1,18 +1,26 @@
 #here's to generate me a number that always going up
 def user_input():
+        #storing the values so it knows how much to generate and to reset the counting point
         reset_point = 3
         reset_value = 0
         count = 0 
         while True:
+                #opening a loop so it can print as many numbers as the user input | with a limit
                 user = str(input("Tirar senha Y Cancelar N: ")).lower().strip()
-                if user == "y":
-                    user = print(f"Your current number {count}")
-                    count +=1 
+                match user:
+                    case user == "y": 
+                             print(f"Your current number {count}")
+                             count +=1 
+                    case user: 
+                        user == "n":
+
+                if count >= reset_point: #this is where it reset the values that's the reason why i give a value to count store a value | so it stores the reset value and then when it reaches the reset point (the limit) it calls the count as 0 basically reseting the value
+                    count = reset_value
+                    print(f"Reseting counting {reset_value}")
+                    
+                     
+                    
+                    break
                 else:
                      print("Digite Y ou N")
-                if count >= reset_point:
-                    reset_value 
-                    print(f"Reseting counting {reset_value}")
-                if user == "n": 
-                    break
 user_input()        
