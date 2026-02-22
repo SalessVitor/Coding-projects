f01e395e6416cb9f485dae229ca1f77527998c9e
Vitor Sales <vitorsaless10@gmail.com>
2026-02-22 19:42:36 -0300

    now the code works clean and its commented this is the last commit

--- waiting_line.py
+++ waiting_line.py
@@ -1,6 +1,6 @@
 #here's to generate me a number that always going up
 def user_input():
-        #storing the values so it knows how much to generate and to reset the counting point
+        #storing the values so it knows how many to generate and to reset the counting point
         reset_point = 3
         reset_value = 0
         count = 0 
@@ -8,19 +8,19 @@ def user_input():
                 #opening a loop so it can print as many numbers as the user input | with a limit
                 user = str(input("Tirar senha Y Cancelar N: ")).lower().strip()
                 match user:
-                    case user == "y": 
-                             print(f"Your current number {count}")
-                             count +=1 
-                    case user: 
-                        user == "n":
-
-                if count >= reset_point: #this is where it reset the values that's the reason why i give a value to count store a value | so it stores the reset value and then when it reaches the reset point (the limit) it calls the count as 0 basically reseting the value
+                    case 'y':
+                        user == "y" 
+                        print(f"Your current number {count}")
+                        count +=1 
+                    case 'n' : 
+                        user == "n"
+                        print("Muito obrigado pela preferência volte sempre")
+                        break
+                    case _ :
+                          print("Digite Y para imprimir N para cancelar")
+                                              
+#this is where it reset the values that's the reason why i give a value to count store a value | so it stores the reset value and then when it reaches the reset point (the limit) it calls the count as 0 basically reseting the value            
+                if count >= reset_point: 
                     count = reset_value
-                    print(f"Reseting counting {reset_value}")
-                    
-                     
-                    
-                    break
-                else:
-                     print("Digite Y ou N")
+                    # print(f"Reseting counting {reset_value}") # it's a back-end it doesn't need to print the reset point
 user_input()        
